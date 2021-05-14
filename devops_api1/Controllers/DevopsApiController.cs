89f50e092ed3f3198d3aa9d7ca59d7b7f7149879
j0k3r <j0k3r@yandex.ru>
2021-05-14 15:27:33 +0300

    controller change

---
+++ devops_api1/Controllers/DevopsApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace devops_api1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DevopsApiController : ControllerBase
+    {
+        private readonly ILogger<DevopsApiController> _logger;
+
+        public DevopsApiController(ILogger<DevopsApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = $"{Assembly.GetExecutingAssembly().GetName()} {Assembly.GetExecutingAssembly().ImageRuntimeVersion} {DateTime.UtcNow:F}";
+            
+            return new ObjectResult(response);
+        }
+    }
+}
