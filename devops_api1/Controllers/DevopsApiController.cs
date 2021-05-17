2c0cb901a704419a78b7917134cc7657e8d24514
j0k3r <j0k3r@yandex.ru>
2021-05-17 15:16:17 +0300

    swagger added

--- devops_api1/Controllers/DevopsApiController.cs
+++ devops_api1/Controllers/DevopsApiController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
 using System.Reflection;
 using System.Threading.Tasks;
 
--- devops_api1/Startup.cs
+++ devops_api1/Startup.cs
@@ -27,6 +27,9 @@ namespace devops_api1
         {
 
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +41,16 @@ namespace devops_api1
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevOps API V1");
+            });
 
             app.UseRouting();
 
